b8a4710bf1de379808df4d693531e6cd206761d6
moohalem <mohalem.public@gmail.com>
2025-06-11 11:16:46 +0700

    Enhance game logic and user experience in Random Number Game
    - Improved credit system: credits are now added for correct guesses and deducted for incorrect ones.
    - Updated game instructions for clarity.
    - Added option to continue or end the game based on credit.

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-## sbt project compiled with Scala 3
+# Random Number Game
 
-### Usage
+A simple Scala 3 console game where you guess a randomly generated number. Your credit increases with correct guesses and decreases with incorrect ones. The game ends when your credit reaches zero or you choose to stop.
 
-This is a normal sbt project. You can compile code with `sbt compile`, run it with `sbt run`, and `sbt console` will start a Scala 3 REPL.
+## Features
 
-For more information on the sbt-dotty plugin, see the
-[scala3-example-project](https://github.com/scala/scala3-example-project/blob/main/README.md).
+- Random number guessing between 1 and 10
+- Credit system: win or lose points based on your guesses
+- Option to play multiple rounds
+
+## Getting Started
+
+### Prerequisites
+
+- [Scala 3](https://www.scala-lang.org/download/)
+- [sbt](https://www.scala-sbt.org/download.html)
+
+### Running the Game
+
+Clone the repository and run:
+
+```sh
+sbt run
+```
+
+### Running Tests
+
+To run the test suite:
+
+```sh
+sbt test
+```
+
+## Project Structure
+
+- `src/main/scala/Main.scala` - Main game logic
+- `src/test/scala/MySuite.scala` - Unit tests
+
+## License
+
+This project is licensed under the MIT License.
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,24 +8,38 @@ def randomGenerator(max: Int): Int = {
 }
 
 @main def hello(): Unit =
+  var credit = 10
   val max = 10
-  val run = 1
-  
-  while(run == 1){
-    val random = randomGenerator(max)
+  var run = 1
 
-    println("Welcome to the guessing game!")
-    
+  println("WELCOME TO THE GUESSING GAME!")
+  Thread.sleep(3000)
+
+  while(run == 1){
+    print("\u001b[2J\u001b[H")
+    val random = randomGenerator(max)
+    println("Every correct guess will be added to your credit but false guess will deduce your credit. 0 credit means you're out")
+    println()
+    println(s"Your credit is $credit")
+    println("---------------------------------------")
     println(s"Please input your number (1 - $max)")
     val userGuess = StdIn.readInt()
     
     if (userGuess != random){
       println(s"I'm sorry your guess' is not correct. The correct answer is $random")
+      credit-=1
     } else {
       println("Yes! Your guess' is correct!")
+      credit+=random
     }
-    val userTry = StdIn.readLine("Do you want to try again? Y/n")
-    if (userTry == "n") {
-      return
+    if (credit==0){
+      println("You're dead!")
+      run = 0
+    }else{
+      val userTry = StdIn.readLine("Do you want to try again? Y/n")
+      if (userTry == "n") {
+      run = 0
+      }
     }
   }
+  return
